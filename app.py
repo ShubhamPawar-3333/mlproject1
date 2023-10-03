7786e8a7a315433a546f4f7104dc9ba34b05be4b
Shubham Pawar <shubham.d.pawar.3333@gmail.com>
2023-10-03 21:07:19 +0530

    Logging and Exception files update

---
+++ app.py
@@ -0,0 +1,8 @@
+import sys
+from src.mlproject.logger import logging
+from src.mlproject.exception import CustomException
+
+
+
+if __name__ == "__main__":
+    logging.info("The execution has started")
